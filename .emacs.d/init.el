3fbe429b5bd7f79464240daa310c3e516a77eced
hightoro_lab <ayanokouji11@gmail.com>
2013-05-24 16:13:00 +0900

    from server

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -55,9 +55,15 @@
 ;;; 自動スペルチェック
 (setq-default flyspell-mode t) (setq ispell-dictionary "american")
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; menu-bar & title & mode line ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; タイトルバーにファイル名を表示
 (setq frame-title-format
       (concat  "%b - emacs@" system-name))
+
+
 
 ;;;;;;;;;;;;;;;;;;;
 ;;; color theme ;;;
@@ -80,6 +86,23 @@
 
 ;;; 終了時にオートセーブファイルを消す 
 (setq delete-auto-save-files t)
+
+;;; バックアップファイルの保存先指定
+(setq backup-directory-alist
+  (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
+    backup-directory-alist))
+
+(setq version-control t)
+(setq kept-new-versions 5)
+;(setq kept-old-versions 5)
+(setq delete-old-versions t)
+
+;;;;;;;;;;;;;;;;;
+;;; clipboard ;;;
+;;;;;;;;;;;;;;;;;
+;;; クリップボードの共有
+(setq x-select-enable-clipborad t)
+(global-set-key "\C-y" 'x-clipborad-yank)
 
 ;;;;;;;;;;;;;;;
 ;;; control ;;;
