34bb4990b8d560bfb803e495246d524265e57a3c
hightoro <ayanokouji11@gmail.com>
2014-07-18 22:50:11 +0900

    add popwin, direx

---
+++ .emacs.d/inits/31_direx.el
@@ -0,0 +1,9 @@
+(require 'direx)
+
+(setq direx:leaf-icon "  "
+      direx:open-icon "- "
+      direx:closed-icon "+ ")
+(push '(direx:direx-mode :position left :width 40 :dedicated t)
+      popwin:special-display-config)
+
+(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
