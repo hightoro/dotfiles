130567828b9fcb7519ec391675bd22846b4d3af6
hightoro <ayanokouji11@gmail.com>
2014-08-27 16:09:24 +0900

    modify setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# empty-directory
+mkdir .emacs.d/buckup
+mkdir .vim/buckup
+
+# create symbolic-link in $HOME
 ln -sfv ${PWD}/.gitignore ~/.gitignore
 ln -sfv ${PWD}/.gitconfig ~/.gitconfig
 ln -sfv ${PWD}/.tmux.conf ~/.tmux.conf
@@ -8,3 +13,5 @@ ln -sfv ${PWD}/.bashrc    ~/.bashrc
 ln -sfv ${PWD}/.bash.d    ~/.bash.d
 ln -sfv ${PWD}/.vimrc     ~/.vimrc
 ln -sfv ${PWD}/.vim       ~/.vim
+ln -sfv ${PWD}/.emacs.d   ~/.emacs.d
+ln -sfv ${PWD}/.zsh.d     ~/.zsh.d
