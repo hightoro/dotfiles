7bc5ae915b651325e6f3eb15a36a297942d14ed2
hightoro <ayanokouji11@gmail.com>
2015-02-18 22:51:06 +0900

    start xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,82 @@
+-- ~/.xmonad/xmonad.hs
+import XMonad
+import XMonad.Layout
+import XMonad.Layout.IM
+import XMonad.Layout.Named
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Spacing           -- this makes smart space around windows
+import XMonad.Util.EZConfig            -- removeKeys, additionalKeys
+import XMonad.Util.Run(spawnPipe)      -- spawnPipe, hPutStrLn
+import XMonad.Layout.Magnifier         -- this makes window bigger
+import XMonad.Layout.ResizableTile     -- Resizable Horizontal border
+import XMonad.Layout.ThreeColumns      -- for many windows
+import Control.Monad (liftM2)          -- myManageHookShift
+import qualified XMonad.StackSet as W  -- myManageHookShift
+import System.IO                       -- for xmobar
+import XMonad.Hooks.DynamicLog         -- for xmobar
+import XMonad.Hooks.ManageDocks        -- avoid xmobar area
+import XMonad.Layout.DragPane          -- see only two window
+import XMonad.Layout.ToggleLayouts     -- Full window at any time
+import XMonad.Layout.NoBorders         -- In Full mode, border is no use
+
+-- main
+main = do
+     xmproc <- spawnPipe "xmobar ~/.xmonad/xmobarrc"
+     xmonad defaultConfig
+		{ terminal           = myTerminal
+		, modMask            = myModMask
+		, borderWidth        = myBorderWidth
+        , normalBorderColor  = "#5a908e"
+        , focusedBorderColor = "#99cb81"
+   -- key bindings
+   -- hooks, layouts
+        , manageHook         = myManageHook
+        , layoutHook         = myLayoutHook
+        , logHook            = myLogHook xmproc
+        , startupHook        = myStartupHook
+        }
+
+
+-- my settings
+myTerminal     = "urxvt"   -- light weight terminal "rxvt-unicode"
+myModMask      = mod4Mask  -- Home / Super_L
+myBorderWidth  = 2
+myLayoutHook   = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout   -- any time Full mode, avoid xmobar area
+myManageHook   = myManageShift <+> myManageFloat <+> manageDocks
+myLogHook b    = dynamicLogWithPP $ myPP b
+myStartupHook  = myStartup
+
+
+-- Handle Window behaveior
+myLayout = (spacing 8 $ ResizableTall 1 (3/100) (1/2) [])
+             |||  (magnifiercz 1.2 (ThreeCol 1 (3/100) (1/2)))
+             |||  (spacing 2 $ (dragPane Horizontal (1/10) (1/2))
+                  ||| (dragPane Vertical   (1/10) (1/2)))
+
+
+-- some window must created there
+myManageShift = composeAll
+            -- if you want to know className, type "$ xprop|grep CLASS" on shell
+            [ className =? "Firefox" --> viewShift "3"
+            , className =? "Opera"   --> viewShift "3"
+            ]
+            where viewShift = doF . liftM2 (.) W.view W.shift
+
+
+-- new window will created in Float mode
+myManageFloat = composeAll
+            [ className =? "MPlayer" --> doFloat
+            , className =? "Gimp"    --> doFloat]
+
+
+-- xmobar setting
+myPP b = xmobarPP
+       { ppOutput = hPutStrLn b
+       , ppTitle  = xmobarColor "green" "" . shorten 50
+       }
+--myPP = xmobarPP {ppTitle  = xmobarColor "green" "" . shorten 50}
+
+-- Start up (at xmonad beggining), like "wallpaper" or so on
+myStartup = do
+        spawn "feh --bg-fill ~/Pictures/wallpaper/WoodHouse.jpg"
+        spawn "compton --config ~/.config/compton.conf"
