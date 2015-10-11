ff6b8b0b5cacab2f79a90a910c1efd76fa15074d
hightoro <ayanokouji11@gmail.com>
2015-10-11 20:30:25 +0900

    encode each envirment

--- .emacs.d/inits/00_init.el
+++ .emacs.d/inits/00_init.el
@@ -2,20 +2,39 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; enveroment ( encode ) ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq-default buffer-file-coding-system 'utf-8-unix)
-(setq-default default-file-name-coding-system 'utf-8-unix)
-(setq-default default-keyboard-coding-system 'utf-8-unix)
-(setq-default default-process-coding-system '(utf-8-unix . utf-8-unix))
-(setq-default default-sendmail-coding-system 'utf-8-unix)
-(setq-default default-terminal-coding-system 'utf-8-unix)
+;; ベースは Shift-JIS のまま
+(set-language-environment "Japanese")
+;See more at: http://yohshiy.blog.fc2.com/blog-entry-273.html#sthash.mgrmsDgY.dpuf
+
+;gnu/linux
+if (eq system-type 'gnu/linux)
+    (
+     (prefer-coding-system 'utf-8-unix)
+     (setq-default buffer-file-coding-system 'utf-8-unix)
+     (setq-default default-file-name-coding-system 'utf-8-unix)
+     (setq-default default-keyboard-coding-system 'utf-8-unix)
+     (setq-default default-process-coding-system '(utf-8-unix . utf-8-unix))
+     (setq-default default-sendmail-coding-system 'utf-8-unix)
+     (setq-default default-terminal-coding-system 'utf-8-unix)
+     ))
+
+;windows
+(if (eq system-type 'windows-nt)
+    (
+     (prefer-coding-system 'utf-8-unix)
+     (setq-default buffer-file-coding-system 'cp932-dos)
+     (setq-default default-file-name-coding-system 'cp932-dos)
+     (setq-default default-keyboard-coding-system 'cp932-dos)
+     (setq-default default-process-coding-system '(cp932-dos . cp932-dos))
+     (setq-default default-sendmail-coding-system 'cp932-dos)
+     (setq-default default-terminal-coding-system 'cp932-dos)
+     )) 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; enveroment ( locale ) ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Localeに合わせた環境の設定(Language)
 (set-locale-environment nil)
-
-
 
 ;;;;;;;;;;;;;;;;;;;;
 ;;; Resent Files ;;;
